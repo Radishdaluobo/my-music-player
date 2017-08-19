c1706833469109acfaff36b815f9df7c74be6903
Radishdaluobo <191245518@qq.com>
2017-08-19 22:59:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from './components/header/header'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+        <div class="icon"></div>
+        <h1 class="text">Chicken Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'header',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/variable.styl"
+    @import "../../common/stylus/mixin.styl"
+
+  .header
+    color :$color-theme
+    font-size: 0
+    text-align center;
+    .icon
+        display: inline-block
+        vertical-align: top
+        width:30px
+        height:32px
+        margin-top: 6px
+        margin-right: 9px
+        background-size: 30px 32px
+        bg-image('logo')
+    .text
+        display: inline-block
+        vertical-align: top
+        line-height: 44px
+        font-size : $font-size-large 
+</style>
