86ea0d80acba9d225635c57bc3141326938d740f
Amy <191245518@qq.com>
2017-08-25 10:18:25 +0800

    add jsonp.js

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import OriginJSONP from 'jsonp'
+
+export default function jsonp(url, data, options) {
+    url += url.indexOf('?') < 0 ? '?' + param(data) : param(data)
+    OriginJSONP(url, options, (err, data) => {
+        return new Promise((resolve, reject) => {
+            if (!err) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
+    })
+}
+
+function param(data) {
+    let params = ''
+    for (var k in data) {
+        data[k] !== undefined ? data[k] : ''
+        params += '&' + k + '=' + encodeURIComponent(data[k])
+    }
+    params = params.slice(1)
+    return params
+}
