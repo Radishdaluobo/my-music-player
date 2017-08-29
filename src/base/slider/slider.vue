d8993f0d9c073c910f284d7f64afb5a54010616c
Amy <191245518@qq.com>
2017-08-29 20:14:03 +0800

    add slider dots

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,11 +4,14 @@
             <slot>
             </slot>
         </div>
+        <div class="dot">
+            <span v-for="(item,index) in dots.length" :class="{'active' : currentPageIndex === index}" class="dot-item"></span>
+        </div>
     </div>
 </template>
 
 <script>
-// import BScroll from 'better-scroll'
+import BScroll from 'better-scroll'
 import { addClass } from '../../common/js/dom.js'
 export default {
     props: {
@@ -27,26 +30,24 @@ export default {
     },
     data() {
         return {
-
+            dots: [],
+            currentPageIndex: 0
         }
     },
     // 为了保证DOM已经渲染完成
     mounted() {
         setTimeout(() => {
             this._setSliderWidth()
+            this._initDots()
             this._initSlider()
         }, 20)
         // 一般浏览器刷新是17ms一次，20ms是经验值
     },
     methods: {
         _setSliderWidth() {
-            console.log('setSliderWidth')
             this.children = this.$refs.sliderGroup.children
-            console.log('this.children', this.children)
             let width = 0
             let sliderWidth = this.$refs.slider.clientWidth
-            console.log(sliderWidth)
-            console.log(this.children.length)
             for (let i = 0; i < this.children.length; i++) {
                 let child = this.children[i]
                 addClass(child, 'slider-item')
@@ -56,8 +57,25 @@ export default {
             this.loop ? width += 2 * sliderWidth : width
             this.$refs.sliderGroup.style.width = width + 'px'
         },
+        _initDots() {
+            this.dots = new Array(this.children.length)
+        },
         _initSlider() {
-
+            this.slider = new BScroll(this.$refs.slider, {
+                scrollX: true,
+                scrollY: false,
+                click: true,
+                momentum: false,
+                snap: true,
+                snapLoop: this.loop,
+                snapThreshold: 0.3,
+                snapSpeed: 400
+            })
+            this.slider.on('scrollEnd', () => {
+                let currentPage = this.slider.getCurrentPage().pageX
+                // console.log('currentPage', currentPage)
+                this.currentPageIndex = currentPage
+            })
         }
     }
 }
@@ -76,4 +94,11 @@ export default {
                 width:100%
                 img
                     width:100%
+    .dot
+        .dot-item
+            display :inline-block
+            background :#fff
+            width: 6px
+            height: 6px
+            border-radius :50%
 </style>
