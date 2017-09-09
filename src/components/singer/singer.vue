a642cf241153b9f058abccea9450cf6ec6eb013e
Radishdaluobo <191245518@qq.com>
2017-09-09 11:44:38 +0800

    get singer List

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,6 +7,9 @@
 <script>
 import { getSingerList } from '../../api/singer'
 import { ERR_OK } from 'common/js/config'
+import Singer from 'common/js/singer'
+const HOT_NAME = '热门歌手'
+const HOT_SINGER_LEN = 10
 export default {
     data() {
         return {
@@ -21,8 +24,25 @@ export default {
             getSingerList().then((res) => {
                 if (res.code === ERR_OK) {
                     console.log(res.data.list)
+                    this._normalizeSinger(res.data.list)
                 }
             })
+        },
+        _normalizeSinger(list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    item: []
+                }
+            }
+            // let singer = {}
+            list.forEach((item, index) => {
+                if (map.hot.item.length < HOT_SINGER_LEN) {
+                    map.hot.item.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+                }
+            })
+             console.log(map.hot)
+
         }
     }
 }
