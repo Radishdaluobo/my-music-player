978ee274dc0e2aa2ccb1ab9b1ae26b78c18c7232
Radishdaluobo <191245518@qq.com>
2017-11-06 19:54:29 +0800

    add animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,84 +1,118 @@
 <template>
     <div class="player" v-show="playList.length>0">
-        <div class="normal-player" v-show="fullScreen">
-            <div class="background">
-                <img width="100%" height="100%" :src="currentSong.image" />
-            </div>
-            <div class="top">
-                <div class="back">
-                    <i class="icon-back"></i>
+        <transition name="normal">
+            <div class="normal-player" v-show="fullScreen">
+                <div class="background">
+                    <img width="100%" height="100%" :src="currentSong.image" />
                 </div>
-                <h2 class="title" v-html="currentSong.name"></h2>
-                <h3 class="subtitle" v-html="currentSong.singer"></h3>
-            </div>
-            <div class="middle">
-                <div class="middle-l">
-                    <div class="cd-wrapper">
-                        <div class="cd"></div>
+                <div class="top">
+                    <div class="back" @click="hideFullScreen">
+                        <i class="icon-back"></i>
                     </div>
-                    <div class="playing-lyric-wrapper">
-                        <div class="playing-lyric"></div>
+                    <h2 class="title" v-html="currentSong.name"></h2>
+                    <h3 class="subtitle" v-html="currentSong.singer"></h3>
+                </div>
+                <div class="middle">
+                    <div class="middle-l">
+                        <div class="cd-wrapper">
+                            <div class="cd">
+                                <img class="cd-image" :src="currentSong.image" />
+                            </div>
+                        </div>
+                        <div class="playing-lyric-wrapper">
+                            <div class="playing-lyric"></div>
+                        </div>
+                    </div>
+                    <div class="middle-r">
+                        <div class="lyric-wrapper">
+                            <div class="text"></div>
+                        </div>
                     </div>
                 </div>
-                <div class="middle-r"></div>
-            </div>
-            <div class="bottom">
-                <div class="dot-wrapper">
-                    <b class="dot"></b>
-                    <b class="dot"></b>
-                </div>
-                <div class="progress-wrapper">
-                    <span class="time time-l"></span>
-                    <div class="progress-bar-wrapper">
-                        <div class="progress-bar">
-                            <div class="bar-inner">
-                                <div class="progress"></div>
-                                <div class="process-btn-wrapper">
-                                    <div class="progress-btn"></div>
+                <div class="bottom">
+                    <div class="dot-wrapper">
+                        <b class="dot active"></b>
+                        <b class="dot"></b>
+                    </div>
+                    <div class="progress-wrapper">
+                        <span class="time time-l">1:00</span>
+                        <div class="progress-bar-wrapper">
+                            <div class="progress-bar">
+                                <div class="bar-inner">
+                                    <div class="progress"></div>
+                                    <div class="process-btn-wrapper">
+                                        <div class="progress-btn"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="time time-r">4:20</div>
                     </div>
-                    <div class="time time-r"></div>
-                </div>
-                <div class="operators">
-                    <div class="icon i-left">
-                        <i class="icon-sequence"></i>
-                    </div>
-                    <div class="icon i-left">
-                        <i class="icon-sequence"></i>
-                    </div>
-                    <div class="icon i-center">
-                        <i class="needsclick icon-pause"></i>
-                    </div>
-                    <div class="icon i-right">
-                        <i class="icon-sequence"></i>
-                    </div>
-                    <div class="icon i-right">
-                        <i class="icon icon-not-favorite"></i>
+                    <div class="operators">
+                        <div class="icon i-left">
+                            <i class="icon-sequence"></i>
+                        </div>
+                        <div class="icon i-left">
+                            <i class="icon-prev"></i>
+                        </div>
+                        <div class="icon i-center">
+                            <i class="needsclick icon-pause"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon-next"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon icon-not-favorite"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="mini-player" v-show="!fullScreen">
-    
-        </div>
+        </transition>
+        <transition name="mini">
+            <div class="mini-player" v-show="!fullScreen" @click="showFullScreen">
+                <div class="icon">
+                    <div class="image-wrapper">
+                        <img width="40" height="40" :src="currentSong.image" />
+                    </div>
+                </div>
+                <div class="text">
+                    <h2 class="name" v-html="currentSong.name"></h2>
+                    <h3 class="singer" v-html="currentSong.singer"></h3>
+                </div>
+                <div class="control">
+                    <div class="progress-circle">
+                        <i class="icon-mini icon-pause-mini"></i>
+                    </div>
+                </div>
+                <div class="control">
+                    <i class="icon-playlist"></i>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 export default {
     computed: {
-        bgStyle() {
-            return `background: url(${this.currentSong.image}) no-repeat center top  100% 100%`
-        },
         ...mapGetters([
             'playList',
             'currentIndex',
             'fullScreen',
             'currentSong'
         ])
+    },
+    methods: {
+        hideFullScreen() {
+            this.setFullScreen(false)
+        },
+        showFullScreen() {
+            this.setFullScreen(true)
+        },
+        ...mapMutations({
+            setFullScreen: 'SET_FULL_SCREEN'
+        })
     },
     created() {
 
@@ -89,6 +123,7 @@ export default {
 <style scoped lang="stylus" rel="stylesheet/stylus">
     @import "../../common/stylus/variable.styl"
     @import "../../common/stylus/mixin.styl" 
+
     .player
         .normal-player
             position:fixed
@@ -141,5 +176,186 @@ export default {
                 bottom: 170px
                 white-space: nowrap
                 font-size: 0
+                .middle-l
+                    display: inline-block
+                    // 两个行内块元素垂直居中
+                    vertical-align :top
+                    height: 0
+                    width: 100%
+                    // 注意这里的padding-top百分比是基于父元素宽度的,
+                    // 这样才能计算出一个正方形的子元素
+                    padding-top: 80%
+                    position : relative
+                    .cd-wrapper
+                        position: absolute
+                        top:0
+                        left: 10%
+                        width: 80%
+                        height: 100%
+                        .cd 
+                            width: 100%
+                            height: 100%
+                            border: 10px solid rgba(255, 255, 255, 0.1)
+                            border-radius: 50%
+                            box-sizing :border-box
+                            overflow :hidden
+                            .cd-image
+                                margin:-10px 0 0 -10px
+                .middle-r
+                    display :inline-block
+                    vertical-align :top
+                    width: 100%
+                    height: 100%
+                    overflow :hidden
+                    .lyric-wrapper
+                        width: 80%
+                        margin: 0 auto
+                        text-align :center
+                        overflow :hidden
+                        .text
+                            line-height :32px
+                            color: $color-text-l
+                            font-size: $font-size-medium
+                            &.current
+                                color: $color-text
+            .bottom
+                position : absolute
+                bottom : 50px
+                width: 100%
+                .dot-wrapper
+                    text-align :center
+                    font-size :0
+                    .dot
+                        display :inline-block 
+                        vertical-align :middle
+                        margin: 0 4px
+                        width: 8px
+                        height: 8px
+                        border-radius :50%
+                        background: $color-text-l  
+                        &.active 
+                            width: 20px
+                            border-radius :5px
+                            background: $color-text-ll
+                .progress-wrapper
+                    display: flex
+                    width :80%
+                    margin: 0 auto
+                    padding:10px 0
+                    .time
+                        color: $color-text
+                        font-size: $font-size-small
+                        flex:0 0 30px
+                        line-height :30px
+                        &.time-l
+                            text-align: left
+                        &.time-r
+                            text-align: right
+                    .progress-bar-wrapper
+                        flex: 1
+                        .progress-bar
+                            height:30px
+                            display: flex;
+                            align-items: center;
+                            .bar-inner
+                                height: 4px
+                                width:100%
+                                background :$color-background-d
+                                position :relative
+                                .progress
+                                    position :absolute
+                                    top: 0
+                                    left: 0
+                                    height:100%
+                                    width: 35%
+                                    background : $color-theme
+                                .process-btn-wrapper
+                                    position :absolute
+                                    top: -6px
+                                    .progress-btn
+                                        box-sizing :border-box
+                                        width:16px;
+                                        height:16px;
+                                        background:$color-theme
+                                        border: 3px solid $color-text
+                                        border-radius :50%
+                .operators
+                    width: 80%
+                    margin: 0 auto
+                    display :flex
+                    align-items: center
+                    .icon 
+                        flex: 1
+                        color: $color-theme
+                        text-align : center
+                        i 
+                            font-size :30px
+                        & .icon-pause
+                            font-size :46px
+                        &.i-left
+                            text-align :left
+                        &.i-right
+                            text-align :right
+            // 定义动画
+            // 1. 为哪块区域定义动画,就用transition标签将那块区域包起来,并设置name属性
+            // 2. 编写css名,注意用&链接
+            // 3.上面一个写一个transition变化的时间就好了,可能内嵌的其他标签变化方式不一样
+            // 4.下面的定义变化的属性,注意与不同标签配合使用,没有则作用于整个的父标签
+            &.normal-enter-active, &.normal-leave-active
+                transition: all 0.3s
+                .top, .bottom
+                    transition : all 0.3s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+            &.normal-enter, &.normal-leave-to
+                opacity :0
+                .top 
+                    transform: translate3d(0, -100px, 0)
+                .bottom
+                    transform: translate3d(0, 100px, 0)
+        .mini-player
+            position :fixed
+            background :$color-highlight-background
+            left:0
+            width: 100%
+            bottom: 0
+            height: 50px
+            z-index: 150
+            display :flex
+            align-items: center
+            .icon
+                flex:0 0 40px
+                padding:0 10px 0 20px
+                .image-wrapper
+                 border-radius :50%
+                 overflow :hidden
+            .text
+                flex: 1
+                display :flex
+                flex-direction: column
+                justify-content: center
+                overflow :hidden
+                .name
+                    no-wrap()
+                    font-size: $font-size-medium
+                    color: $color-text
+                    margin-bottom: 2px
+                .singer
+                    no-wrap()
+                    font-size: $font-size-small
+                    color: $color-text-d
+            .control
+                flex:0 0 30px
+                padding: 0 10px
+                .progress-circle
+                    position :relative
+                    .icon-mini
+                        font-size :32px 
+                        color: $color-theme-d
+                .icon-playlist
+                    font-size: 30px
+                    color: $color-theme-d
+            &.mini-enter-active, &.mini-leave-active
+                transition: all 0.4s
+            &.mini-enter, &.mini-leave-to
+                opacity : 0
 
 </style>
